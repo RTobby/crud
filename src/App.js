363366a9df95b21a05a9d8acbc1616a3ff2d5c47
RTobby <tolkachr@gmail.com>
2021-05-28 14:49:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Table from './components/table/Table';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: "",
+      fetch: false
+    }
+  }
+
+  getFetch = () => {
+    fetch('http://178.128.196.163:3000/api/records', { method: 'GET' })
+      .then(response => response.json())
+      .then(data => this.setState({ persons: data, fetch: !this.state.fetch }))
+  }
+
+  componentDidMount() {
+    this.getFetch();
+    this.timer = setInterval(() => this.getFetch(), 100);
+  }
+  componentWillUnmount() {
+    this.timer = null;
+  }
+
+  addNewRow = (text) => {
+    let data = { data: { name: text } }
+    fetch('http://178.128.196.163:3000/api/records', {
+      method: 'PUT',
+      headers: {
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+  }
+
+  deleteRow = i => {
+    fetch(`http://178.128.196.163:3000/api/records/${i}`, { method: 'delete' })
+      .then(response => response.json())
+  };
+
+  updateRowContent = (name, secondName, i) => {
+    let data = {
+      data: {
+        name: name,
+        secondName: secondName,
+      }
+    };
+
+    fetch(`http://178.128.196.163:3000/api/records/${i}`,
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+    
+  };
+
+  setRow = (item) => {
+    return (
+      <div>
+        <Table key={item._id} index={item._id} name={item.data.name} secondName={item.data.secondName} update={this.updateRowContent} remove={this.deleteRow} />
+      </div>
+    );
+  };
+
+
+
+  render() {
+    if (!this.state.persons.length) return null;
+    return (
+      <div className="App">
+        <div className="wrapp">
+          <div >{this.state.persons.map(this.setRow)}</div>
+          <button className="save-btn" onClick={this.addNewRow('Name')}>Add</button>
+        </div>
+      </div>
+    );
+  }
+};
+
+
+
+
+
+
+
+export default App;
---
+++ src/components/table/Table.js
@@ -0,0 +1,74 @@
+import React from "react"
+
+
+
+class Table extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editable: false,
+            name:this.props.name,
+            secondName: this.props.secondName,
+        }
+    }
+
+        edit = () => {
+            this.setState({ editable: true });
+        };
+
+        remove = () => {
+            this.props.remove(this.props.index);
+        };
+
+        save = () => {
+            this.props.update(this.state.name, this.state.secondName, this.props.index)
+            this.setState({ editable: false });
+        };
+
+        setEditableRow = () => {
+            return (
+                <tr className="row">
+                    <th> <input className="posts-form__input" ref="newName" defaultValue={this.props.name} 
+                    onChange={ev=>{this.setState({name:ev.target.value})}}/> </th>
+                    <th>  <input className="posts-form__input" ref="newSecondName" defaultValue={this.props.id} 
+                     onChange={ev=>{this.setState({secondName:ev.target.value})}}/> </th>
+                    <th>
+                        <button className="" onClick={this.save} >
+                            Save
+                       </button>
+                    </th>
+                </tr>
+
+            );
+        };
+
+        setUneditableRow = () => {
+            return (
+                <tr className="row">
+                    <th> <input className="posts-form__input" defaultValue={this.props.name} disabled='disabled' /> </th>
+                    <th> <input className="posts-form__input" defaultValue={this.props.secondName} disabled='disabled' /> </th>
+                    <th>
+                        <div className="buttons">
+                            <button onClick={this.edit}>
+                                Edit
+                                </button>
+                            <button onClick={this.remove}>
+                                Delete
+                                </button>
+                        </div>
+                    </th>
+                </tr>
+            );
+        };
+    
+    render() {
+       // <Header/>
+        if (this.state.editable) {
+            return this.setEditableRow();
+        } else {
+            return this.setUneditableRow();
+        }
+    }
+}
+
+export default Table;
